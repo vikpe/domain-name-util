f2a22da56899ae623142e00933a5e23ddfb553c8
Viktor Persson <viktor.persson@arcsin.se>
2019-09-12 16:16:31 +0200

    Reformat code using prettier.

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ export const regExps = {
   partlyNumeric: /\d+/g
 };
 
-export const containsAlphabetics = domainName => matchesRegExp(domainName, regExps.partlyAlphabetic);
-export const containsNumerics = domainName => matchesRegExp(domainName, regExps.partlyNumeric);
-export const getDomains = domainName => domainName.split('.');
-export const getLength = domainName => getBaseLevelDomain(domainName).replace('.', '').length;
-export const isAlphabetic = domainName => matchesRegExp(domainName, regExps.strictlyAlphabetic);
-export const isHyphenated = domainName => domainName.includes('-');
-export const isNumeric = domainName => matchesRegExp(domainName, regExps.strictlyNumeric);
+export const containsAlphabetics = domainName => matchesRegExp(domainName,
+  regExps.partlyAlphabetic);
+export const containsNumerics = domainName => matchesRegExp(domainName,
+  regExps.partlyNumeric);
+export const getDomains = domainName => domainName.split(".");
+export const getLength = domainName => getBaseLevelDomain(domainName).
+  replace(".", "").length;
+export const isAlphabetic = domainName => matchesRegExp(domainName,
+  regExps.strictlyAlphabetic);
+export const isHyphenated = domainName => domainName.includes("-");
+export const isNumeric = domainName => matchesRegExp(domainName,
+  regExps.strictlyNumeric);
 export const isSubdomain = domainName => getDomains(domainName).length > 2;
 
 export const matchesRegExp = (domainName, regexp) => {
-  const baseLevelDomainStr = getBaseLevelDomain(domainName).replace('.', '');
+  const baseLevelDomainStr = getBaseLevelDomain(domainName).replace(".", "");
   return null !== baseLevelDomainStr.match(regexp);
 };
 
 export const getBaseLevelDomain = domainName => {
   const domains = getDomains(domainName);
-  return domains.slice(0, domains.length - 1).join('.');
+  return domains.slice(0, domains.length - 1).join(".");
 };
 
 export const getTopLevelDomain = domainName => {
